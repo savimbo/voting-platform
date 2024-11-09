7298a3dcb7332b80729dae7c0b48682dc147b8e7
Manolo-Esc <manolo.escribano@gmail.com>
2024-11-09 19:31:04 +0100

    initial release

--- vote-api/src/auth/model/token-payload.ts
+++
@@ -1,8 +0,0 @@
-
-// Custom unformation embedded in the jwt auth tokens
-
-export class AuthTokenPayload {
-    user_id: string;
-    user_email: string;
-    user_lang: string;
-}
--- vote-api/src/constants/error-code.ts
+++
@@ -1,5 +0,0 @@
-export enum ErrorCode {
-    LOGIN_NAME_ERROR = "1000",
-    LOGIN_PASSWORD_ERROR = "1001",
-    SIGNUP_REGISTER_ERROR = "1002",
-}
